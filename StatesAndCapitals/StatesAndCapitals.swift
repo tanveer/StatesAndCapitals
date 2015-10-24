67ff3b7a3c1dc09c901ca3ae356c6301ad662919
tmbashir <ifarting@gmail.com>
2015-10-23 21:50:12 -0400

    Add .gitignore

---
+++ StatesAndCapitals/StatesAndCapitals.swift
@@ -0,0 +1,13 @@
+//
+//  StatesAndCapitals.swift
+//  StatesAndCapitals
+//
+//  Created by Tanveer Bashir on 10/23/15.
+//  Copyright © 2015 Tanveer Bashir. All rights reserved.
+//
+
+import UIKit
+
+class StatesAndCapitals: NSObject {
+
+}
